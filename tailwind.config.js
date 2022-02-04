49bb81f80363a71d925b44c0f2f592e6c6508bc9
relara <relvira_7232@stu.southtexascollege.edu>
2022-02-04 13:56:42 -0600

    Finished App

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit",
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
