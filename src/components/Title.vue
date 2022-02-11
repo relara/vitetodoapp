5cee0d47c4bfd599708455bfbbb233c2080d2dac
relara <relvira_7232@stu.southtexascollege.edu>
2022-02-10 19:37:08 -0600

    Refactor Done

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+import useTodos from "../composables/useTodos";
+
+const { addTodo } = useTodos();
+
+const newTodo = ref("");
+const add = () => {
+  if (newTodo.value) {
+    addTodo(newTodo.value);
+    newTodo.value = "";
+  }
+};
+</script>
+
+<template>
+    <h1 class="pb-4 text-6xl font-thin tracking-tight text-center text-gray-300">👽My Todo App👽</h1>
+      <input @change="add" v-model="newTodo" type="text" class="px-4 py-2 text-center rounded-lg" placeholder="New Todo"/>
+</template>
